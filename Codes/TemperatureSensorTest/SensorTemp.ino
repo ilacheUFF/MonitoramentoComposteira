595ec91d8d563b85cf8152e930751a561411e53e
ilacheUFF <65617924+ilacheUFF@users.noreply.github.com>
2022-09-23 12:08:36 -0300

    Create SensorTemp.ino
    Versão para dois sensores de temperatura , fonte https://www.filipeflop.com/blog/sensor-de-temperatura-ds18b20-arduino/#:~:text=O%20sensor%20de%20temperatura%20DS18B20,opera%C3%A7%C3%A3o%3A%203%20a%205%2C5V

---
+++ Codes/TemperatureSensorTest/SensorTemp.ino
@@ -0,0 +1,97 @@
+// Programa : Sensor de temperatura DS18B20
+// Autor : FILIPEFLOP
+// Adaptação : UFF / TER / Larissa / Ivanovich
+// Biblioteca OneWire baixada diretamente do arduino
+//Biblioteca DAllas, baixada do site FilipeFlop
+//
+ 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Porta do pino de sinal do DS18B20
+#define ONE_WIRE_BUS D2
+ 
+// Define uma instancia do oneWire para comunicacao com o sensor
+OneWire oneWire(ONE_WIRE_BUS);
+ 
+// Armazena temperaturas minima e maxima
+float tempMin = 999;
+float tempMax = 0;
+ 
+DallasTemperature sensors(&oneWire);
+DeviceAddress sensor1,sensor2;
+bool primeira = false;
+ 
+void setup(void)
+{
+  Serial.begin(9600);
+  sensors.begin();
+  // Localiza e mostra enderecos dos sensores
+  
+  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(LED_BUILTIN,LOW);
+  
+   
+}
+ 
+void mostra_endereco_sensor(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    // Adiciona zeros se necessário
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+ 
+void loop()
+{
+
+  if (primeira == false)
+  {
+  Serial.println("Localizando sensores DS18B20...");
+  Serial.print("Foram encontrados ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" sensores.");
+  if (!sensors.getAddress(sensor1, 0)) 
+     Serial.println("Sensor1 nao encontrado !"); 
+  if (!sensors.getAddress(sensor2, 1)) 
+     Serial.println("Sensor2 nao encontrado !"); 
+  // Mostra o endereco do sensor encontrado no barramento
+  Serial.print("Endereco sensor1: ");
+  mostra_endereco_sensor(sensor1);
+  Serial.print("Endereco sensor2: ");
+  mostra_endereco_sensor(sensor2);
+  Serial.println();
+  Serial.println();
+  primeira = true;
+    
+  }
+  // Le a informacao do sensor
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempC(sensor1);
+  float tempC2 = sensors.getTempC(sensor2);
+  // Atualiza temperaturas minima e maxima
+  if (tempC < tempMin)
+  {
+    tempMin = tempC;
+  }
+  if (tempC > tempMax)
+  {
+    tempMax = tempC;
+  }
+  // Mostra dados no serial monitor
+  Serial.print("Temp S1 C: ");
+  Serial.print(tempC);
+  Serial.print(" Min : ");
+  Serial.print(tempMin);
+  Serial.print(" Max : ");
+  Serial.println(tempMax);
+  Serial.print("Temp S2 C: ");
+  Serial.println(tempC2);
+   
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(1500);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(1500);
+}
